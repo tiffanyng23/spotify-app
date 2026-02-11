d8f974d6450bd22eec512f011984a91135dbb1f3
tiffanyng23 <ngtiffany23@gmail.com>
2026-02-10 21:59:37 -0500

    added customization to graph and created readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # spotify-app
-Dashboard providing album and track information for any artist on Spotify
+A reimagined Spotify Artist Explore Page where you can search album and track information for any artist on Spotify.
+
+
+## Why did I make this page?
+Sometimes I find that I don't know where to start when wanting to explore the music catalogue of a new artist. The artist's page on Spotify begins with a list of 10 of their current most popular tracks, and is followed by a list of their discography and some playlists they are featured in. I find at first glance, it can be difficult to know where to start (especially if they have a lot of albums!). 
+
+I created a reimagined Spotify Artist Explore Page to helps users decide where to start when exploring a new artist. It has a lot of the same components seen on Spotify, but focuses more on exploring artist albums and tracks that are from albums. I wanted this page to focus on albums, and tracks found in their albums (not so much songs they are featured in, extra singles, or from compilation albums), since I feel albums show more of the artists artistic/musical vision. Personally, I prefer listening to albums over playlists since I like exploring overarching themes, transitions, and sounds across tracks in an album. This reflects how I approached the layout and focus of this page. 
+
+The criteria used to organize these songs and tracks is somewhat based on Spotify's popularity score which is defined on the Spotify Web API page as: 
+
+"The value will be between 0 and 100, with 100 being the most popular. The popularity of a track is a value between 0 and 100, with 100 being the most popular. The popularity is calculated by algorithm and is based, in the most part, on the total number of plays the track has had and how recent those plays are. Generally speaking, songs that are being played a lot now will have a higher popularity than songs that were played a lot in the past. Duplicate tracks (e.g. the same track from a single and an album) are rated independently. Artist and album popularity is derived mathematically from track popularity. Note: the popularity value may lag actual popularity by a few days: the value is not updated in real time."
+
+Using the popularity value as a factor for which tracks (and albums) to display is evidentely a method that is not tailored to the specific user, but moreso a way to introduce the user to tracks (and albums) from an artist that are likely considered fan favourites. This is a good way to get an introduction to elements of an artist's music that appeals to their fans. If users want to explore an artists less popular work, this can be done by listening to the "hipster" tracks and album (i.e. lowest popularity value from the artists discography) that are included in the page. I wanted this page to make it easier to navigate and view an artists catalogue.
+
+
+## Components of this Project
+There are 3 tabs: "Albums", "Top Tracks", and "Still not sure what to pick?". 
+
+### Albums Tab
+The first tab "Albums" contains a table where you can sort all the artists albums based on "popularity", album length, and release date. This allows users to find an album that aligns with how they want to explore an artists music (unfortunately I can no longer access genre information for an album, that would have been great to add). For example, if they want to check out their most popular album, or their newest album. Or if they want to check out their earlier work first, or are looking for a shorter album to get a quick glimpse of the artist. At the right end of the table, there is a column which provides links at which the user can click to listen to their album of choice on Spotify.
+
+### Tracks Tab
+The second tab "Tracks" is ideal for those who are not as much album listeners, but more tracks/playlist listeners. The top half of this tab has 3 main sections. The left side lists the artists top 5 most "hyped" tracks while the right side lists the artists top5 most "hipster" tracks. This is determined by the popularity score, where the songs with the top 5 highest score is in the "hyped" tracks list, while the top 5 lowest popularity score is in the "hipster" tracks list. In the middle, there is a customizable list where the user can select the degree of hyped to hipster ratio, which essentially provides a list of tracks with a popularity score that falls in between the left and right lists. At the bottom of this tab there is a customizable bar chart which shows the popularity score of the artists album tracks. Users can filter tracks included based on order (ascending/descending) and number of tracks to display. Each of the tracks displayed on this page are hyperlinked, allowing users to click and listen to the song of interest.
+
+### Not sure what to pick?
+This tab is for users who truly want to randomnly select a track to listen to. On the left side, there is a list of the artist's top tracks. This should match up with the 10 tracks shown on the top of an artists page on Spotify. It likely differs a bit from the "hyped" tracks list in the Tracks Tab since it also considers non-album tracks. On the right side, there is a random song generator. For those who really cannot decide on what to listen to, by clicking the button, it will randomnly select one of the artist's tracks and open the link for the user. 
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from dash import Dash, dash_table, html, dcc, callback, Output, Input, State, no
 import dash_bootstrap_components as dbc
 import plotly.express as px
 from api_requests.access_api import get_token
-from api_requests.artist_albums import albums, album_popularity
+from api_requests.artist_albums import albums, album_popularity_length
 from api_requests.popular_tracks import top_tracks
 from api_requests.search_artist import search
 from api_requests.all_tracks import all_tracks, all_track_uris, chunk_list, tracks_popularity
@@ -13,7 +13,7 @@ import time
 import re
 
 #create app
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
 
 #app layout
@@ -21,11 +21,12 @@ app.layout = dbc.Container([
     dcc.Store(
         id="artist-data"
     ),
-    html.H1(children='Spotify Comprehensive Artist Page'),
-    html.Div(children=" An application where you can gather album and track information for your favourite artists."),
+    html.H1(children="A Reimagined Spotify Artist Explore Page"),
+    html.Div(children=" Want to explore music of a new artist but don't know where to start? Type in the artist's name, click submit, and get exploring!"),
 
     html.Div([dcc.Input(id="artist-input", type="text", value="frank ocean", placeholder="Input Artist Name"),
     html.Button("Submit", id="submit-button", n_clicks=0)]),
+    html.Br(),
     dbc.Row(
             dcc.Tabs(id="artist-tabs", value="tab-1", children=[
             dcc.Tab(label="Albums", value="tab-1", children=[
@@ -42,8 +43,26 @@ app.layout = dbc.Container([
                     )
                 ]),
             dcc.Tab(label="Top Tracks", value="tab-2", children=[
+                    dbc.Row([
+                        dbc.Col([
+                            html.Div([
+                                html.P("Order By:"),
+                                dcc.RadioItems(id="order-radio", options=["Least to Most Popular", "Most to Least Popular"], value= "Most to Least Popular", inline=True),
+                                ]),
+                            ], width=6),
+                        dbc.Col([
+                            html.Div([
+                                html.P("Select Number of Tracks to Display:"),
+                                dcc.RadioItems(id="tracks-radio", options = ['10', '20', '30', '50', 'All'], value = '20', inline=True),
+                            ]),
+                        ], width=6),
+                    ]),
+                    dbc.Row([
+                        dcc.Graph(id="tracks-bar")
+                    ]),
+                ]),
+            dcc.Tab(label="Still not sure where to start?", value="tab-3", children=[
                     html.Div(id="top-tracks"),
-                    dcc.Graph(id="tracks-bar"),
                 ])
             ]), 
         )
@@ -66,7 +85,7 @@ def fetch_data(n_clicks, artist):
     #get all album data from artist uri
     albums_data = albums(token, uri)
     #get album popularity
-    album_pop = album_popularity(token, albums_data)
+    album_pop_length = album_popularity_length(token, albums_data)
     #get top tracks from artist
     top = top_tracks(token, uri)
     #get all tracks from an albums json data
@@ -78,7 +97,7 @@ def fetch_data(n_clicks, artist):
 
     return {
         "albums": albums_data,
-        "album_popularity": album_pop,
+        "album_popularity_length": album_pop_length,
         "top_tracks": top,
         "tracks_df": tracks_df.to_dict("records")
     }
@@ -99,7 +118,7 @@ def album_info(data):
     # key would be album uri, values would be each of the columns
     table_data = []
     #extract popularity scores
-    popularity_scores = data["album_popularity"] #stores in dictionary - album uri:score
+    popularity_length = data["album_popularity_length"] #stores in dictionary - album uri:[score, length]
 
     for album_item in data["albums"]:
         #get album uri key to extract popularity value
@@ -109,17 +128,20 @@ def album_info(data):
 
         #fill table data
         table_data.append(
-            {"Popularity": popularity_scores[uri], #use album uri to get score for specific album
+            {"Popularity": popularity_length[uri][0], #use album uri to get score for specific album
+            "Number of Tracks": popularity_length[uri][1],
             "Name": album_item["name"], 
             "Release Date": album_item["release_date"],
             "Album Link": f"[Listen]({album_item['external_urls']['spotify']})"} #markdown format for link
         )
 
-    #columns - id must match key values, name is what is displayed on the table and can be anything
+    #columns - id must match key values since it's used to populate the table
+    #name is what is displayed on the table and can be anything
     columns = [
-        {"name": "Popularity", "id": "Popularity"},
         {"name": "Album Name", "id": "Name"},
         {"name": "Release Date", "id": "Release Date"},
+        {"name": "Popularity", "id": "Popularity"},
+        {"name": "Number of Tracks", "id": "Number of Tracks"},
         {"name": "Album Link", "id":"Album Link", "presentation":"markdown"},
         ]
     return table_data, columns
@@ -143,24 +165,39 @@ def render_top_tracks(data):
 # gather popularity value for all album tracks
 @callback(
     Output("tracks-bar", "figure"),
-    Input("artist-data", "data")
+    Input("artist-data", "data"),
+    Input("tracks-radio", "value"),
+    Input("order-radio", "value"),
 )
-def tracks_graph(data):
+def tracks_graph(data, number_tracks, order):
     if not data:
         return no_update
 
     #get tracks, popularity data frame
     df = pd.DataFrame(data["tracks_df"])
 
+    #sort tracks by requested order
+    if order == "Least to Most Popular":
+        sorted_df = df.sort_values(by="popularity")
+    else:
+        sorted_df = df.sort_values(by="popularity", ascending=False)
+
+    #display requested number of tracks
+    if number_tracks == "All":
+        filtered_df = sorted_df
+    else:
+        filtered_df = sorted_df.head(int(number_tracks))
+
     #create scatter graph 
     fig = px.bar(
-        df,
+        filtered_df,
         x="track",
         y="popularity",
         hover_data=["track", "popularity"],
-        
+        orientation="v",
     )
     fig.update_xaxes(showticklabels=False)
+
     return fig
 
 #run app
